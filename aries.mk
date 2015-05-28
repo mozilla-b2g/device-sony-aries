3eb0e04bd18ef1ded338447e41964bec71c06286
Seinlin <seinlin.maung@gmail.com>
2015-05-28 11:13:42 +0800

    Bug 1163550 - Add PRODUCT_NAME areis.

--- aries.mk
+++ aries.mk
@@ -1,4 +1,3 @@
-#
 # Copyright 2014 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-add_lunch_combo aosp_d5803-userdebug
-add_lunch_combo aries-userdebug
+$(call inherit-product, device/sony/aries/aosp_d5803.mk)
+
+PRODUCT_NAME := aries
+PRODUCT_DEVICE := aries
+PRODUCT_MODEL := Xperia Z3 Compact (B2G)
