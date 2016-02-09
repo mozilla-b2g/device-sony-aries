2669b6d104f2923b3d9c614247e01c18d46ccf9e
Adam Farden <adam@farden.cz>
2016-02-09 19:30:46 +0100

    We welcome Aries to the B2G community!
    Signed-off-by: Adam Farden <adam@farden.cz>

--- aries.mk
+++ aries.mk
@@ -1,4 +1,3 @@
-#
 # Copyright 2014 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-add_lunch_combo aries-userdebug
+$(call inherit-product, device/sony/aries/aosp_d5803.mk)
+
+PRODUCT_NAME := aries
+PRODUCT_DEVICE := aries
+PRODUCT_MODEL := Xperia Z3 Compact (B2G)
+
+GAIA_DEV_PIXELS_PER_PX := 2.25
+BOOTANIMATION_ASSET_SIZE := 720p
+
+$(call inherit-product-if-exists, vendor/sony/aries-blobs/aries-vendor-blobs.mk)
